2f276a3399384b906a08be6e5a0458fa4e3c6a08
mahi25soni <msdraj02@protonmail.com>
2025-04-11 13:56:38 +0530

    refactor(contactus) : change schema, services

--- dist/services/ContactUsService.js
+++ dist/services/ContactUsService.js
@@ -7,6 +7,7 @@ const ContactUsModel_1 = __importDefault(require("../models/ContactUsModel"));
 const email_1 = require("../libs/email");
 const ContactUsTypes_1 = require("../types/ContactUsTypes");
 const zodHandling_1 = require("../libs/zodHandling");
+const moment_timezone_1 = __importDefault(require("moment-timezone"));
 class ContactUsService {
     async create(data) {
         try {
@@ -15,12 +16,18 @@ class ContactUsService {
                 const message = (0, zodHandling_1.formatZodError)(result.error.errors);
                 throw new Error(message);
             }
-            const nData = await ContactUsModel_1.default.create(data);
+            const { selectedDate, selectedTime, timeZone } = data;
+            const dateTimeString = `${selectedDate} ${selectedTime}`;
+            const isoDateTime = moment_timezone_1.default
+                .tz(dateTimeString, "DD-MM-YYYY hh:mm A", timeZone)
+                .toISOString();
+            const updatedData = { ...data, meetingAt: isoDateTime };
+            const nData = await ContactUsModel_1.default.create(updatedData);
             if (!nData) {
                 throw new Error("Failed to create contact us entry");
             }
-            (0, email_1.sendMeetingInvitation)(data);
-            return nData;
+            (0, email_1.sendMeetingInvitation)(nData);
+            return updatedData;
         }
         catch (error) {
             throw error;
